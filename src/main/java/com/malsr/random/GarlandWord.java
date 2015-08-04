1859caa4ec632b6c1b29a10204d1d60cedf1c42a
MalsR <malinda_rajapakse@hotmail.com>
2015-08-04 21:58:55 +0100

    Changed some of the variable names in Garland Words

--- src/main/java/com/malsr/random/GarlandWord.java
+++ src/main/java/com/malsr/random/GarlandWord.java
@@ -4,12 +4,15 @@ public class GarlandWord {
 
     public int garland(final String wordToCheck) {
         int degreeCount = 0;
+        int lengthOfWord = wordToCheck.length();
 
-        for (int i = 1; i < wordToCheck.length(); i++) {
-            String endWord = wordToCheck.substring(wordToCheck.length() - i, wordToCheck.length());
-            String remainingWord = wordToCheck.substring(i, wordToCheck.length());
+        for (int i = 1; i < lengthOfWord; i++) {
+            String endWord = wordToCheck.substring(lengthOfWord - i, lengthOfWord);
+            String remainingWord = wordToCheck.substring(i, lengthOfWord);
             String combinedWord = endWord + remainingWord;
+
             if (wordToCheck.equalsIgnoreCase(combinedWord)) {
+                //Do not want to break here as we want to calculate max degree of garland words
                 degreeCount = i;
             }
         }
--- src/test/java/com/malsr/random/GarlandWordTest.java
+++ src/test/java/com/malsr/random/GarlandWordTest.java
@@ -42,5 +42,4 @@ public class GarlandWordTest {
 
         assertEquals(5, garlandWord.garland("undergrounder"));
     }
-
 }
