0fec0420ce855499b25c72b9ea6117afabcbad41
Malinda Rajapakse <malinda_rajapakse@hotmail.com>
2015-11-06 21:09:06 +0000

    Moved comments from class to own readme

--- src/main/java/com/malsr/coderbyte/FirstReverse.java
+++ src/main/java/com/malsr/coderbyte/FirstReverse.java
@@ -2,13 +2,6 @@ package com.malsr.coderbyte;
 
 public class FirstReverse {
 
-//    Using the Java language, have the function FirstReverse(str) take the str parameter being passed and
-//    return the string in reversed order.
-//
-//    Examples
-//    Input = "coderbyte"Output = "etybredoc"
-//    Input = "I Love Code"Output = "edoC evoL I"
-
     public String reverse(String str) {
         if (str == null || str.length() == 0) {
             return null;
