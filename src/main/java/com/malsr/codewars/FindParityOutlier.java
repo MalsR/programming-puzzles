978e7131d2488936e5e70d911a10e856f05432c6
MalsR <malinda_rajapakse@hotmail.com>
2016-09-19 22:21:21 +0100

    Added some tests to ParityOutlier with new implementation

--- src/main/java/com/malsr/codewars/FindParityOutlier.java
+++ src/main/java/com/malsr/codewars/FindParityOutlier.java
@@ -1,12 +1,16 @@
 package com.malsr.codewars;
 
-import javax.swing.*;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class FindParityOutlier {
 
     public int find(int[] integers) {
+        if (integers.length < 3) {
+            throw new IllegalArgumentException("Array should contain three numbers or more");
+        }
+
         Set<Integer> evenNumbers = new HashSet<>();
         Set<Integer> oddNumbers = new HashSet<>();
 
@@ -24,7 +28,8 @@ public class FindParityOutlier {
         if (evenNumbers.size() > oddNumbers.size()) {
             return oddNumbers.stream().findFirst().get();
         } else {
-            return evenNumbers.stream().findFirst().get();
+            Optional<Integer> firstEvenNumber = evenNumbers.stream().findFirst();
+            return firstEvenNumber.isPresent() ? firstEvenNumber.get() : 0;
         }
     }
 
--- src/test/java/com/malsr/codewars/FindParityOutlierTest.java
+++ src/test/java/com/malsr/codewars/FindParityOutlierTest.java
@@ -1,16 +1,30 @@
 package com.malsr.codewars;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class FindParityOutlierTest {
 
-    @Test
-    public void returnsZeroWhenNoNumbersPresent() {
-        FindParityOutlier parityOutlier = new FindParityOutlier();
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
-        assertEquals("", 0, parityOutlier.find(new int[]{}));
+    @Test
+    public void throwsExceptionWhenNoNumbersArePresent() {
+        expectedException.expect(IllegalArgumentException.class);
+
+        FindParityOutlier parityOutlier = new FindParityOutlier();
+        parityOutlier.find(new int[]{});
+    }
+
+    @Test
+    public void throwsExceptionWhenLessThanThreeNumbersArePresent() {
+        expectedException.expect(IllegalArgumentException.class);
+
+        FindParityOutlier parityOutlier = new FindParityOutlier();
+        parityOutlier.find(new int[] {2, 9});
     }
 
     @Test
@@ -20,6 +34,15 @@ public class FindParityOutlierTest {
         int[] numbersToFind = new int[] {2, 4, 3, 8, 100, 220};
 
         assertEquals("", 3, parityOutlier.find(numbersToFind));
+    }
+
+    @Test
+    public void returnsTheOddNumberZeroWhenAllOtherNumbersAreEven() {
+        FindParityOutlier parityOutlier = new FindParityOutlier();
+
+        int[] numbersToFind = new int[] {0, 2 , 4, 6};
+
+        assertEquals("", 0, parityOutlier.find(numbersToFind));
     }
 
     @Test
