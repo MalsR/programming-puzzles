ba816878d2f1aa532444d6233eb2e76955ce5993
MalsR <malinda_rajapakse@hotmail.com>
2016-10-04 23:25:33 +0100

    Add another way for reversing fun and refactor wip

--- src/main/java/com/malsr/codewars/ReversingFun.java
+++ src/main/java/com/malsr/codewars/ReversingFun.java
@@ -4,26 +4,44 @@ public class ReversingFun {
 
     public String funReverse(final String stringToReverse) {
 
-        String initialReversedString = reverse(stringToReverse);
+        String initialReversedString = new StringBuilder(stringToReverse).reverse().toString();
 
-        StringBuilder updatedResult = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         int length = stringToReverse.length();
 
-        String firstPart;
-        String toBeReversed;
-
         for (int i = 0; i < length - 2; i++) {
-            firstPart = initialReversedString.substring(0, i+1);
+            String firstPart = initialReversedString.substring(0, i+1);
 
             String secondPart = initialReversedString.substring(i + 1, initialReversedString.length());
-            toBeReversed = reverse(secondPart);
+            String toBeReversed = new StringBuilder(secondPart).reverse().toString();
 
             initialReversedString = firstPart + toBeReversed;
         }
 
-        updatedResult.append(initialReversedString);
+        result.append(initialReversedString);
 
-        return updatedResult.toString();
+        return result.toString();
+    }
+
+    public String funReverse_1(final String stringToReverse) {
+
+        String initialReversedString = reverse(stringToReverse);
+
+        StringBuilder result = new StringBuilder();
+        int length = stringToReverse.length();
+
+        for (int i = 0; i < length - 2; i++) {
+            String firstPart = initialReversedString.substring(0, i+1);
+
+            String secondPart = initialReversedString.substring(i + 1, initialReversedString.length());
+            String toBeReversed = reverse(secondPart);
+
+            initialReversedString = firstPart + toBeReversed;
+        }
+
+        result.append(initialReversedString);
+
+        return result.toString();
     }
 
     private String reverse(String stringToReverse) {
--- src/test/java/com/malsr/codewars/ReversingFunTest.java
+++ src/test/java/com/malsr/codewars/ReversingFunTest.java
@@ -1,15 +1,47 @@
 package com.malsr.codewars;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class ReversingFunTest {
 
+    public String actualValue;
+    public String expectedValue;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {"012345", "504132"},
+                {"012", "201"},
+                {"012345", "504132"},
+                {"hello", "ohlel"},
+                {"4ppso1vdjc9rjyf5bkmd5nztr8", "84rptpzsno51dvmdkjbc59fryj"}
+        });
+    }
+
+    public ReversingFunTest(String actualValue, String expectedValue) {
+        this.actualValue = actualValue;
+        this.expectedValue = expectedValue;
+    }
+
     @Test
-    public void reversesStringUpToSecondIndex() {
+    public void reversesStringByLength() {
         ReversingFun reversingFun = new ReversingFun();
 
-        assertEquals("504132", reversingFun.funReverse("012345"));
+        assertEquals(expectedValue, reversingFun.funReverse(actualValue));
+    }
+
+    @Test
+    public void reversesString_AnotherWay() {
+        ReversingFun reversingFun = new ReversingFun();
+
+        assertEquals(expectedValue, reversingFun.funReverse_1(actualValue));
     }
 }
