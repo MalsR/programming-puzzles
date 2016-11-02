53c34e2cb491d05fa110f05fbea60d83f6925599
MalsR <malinda_rajapakse@hotmail.com>
2016-11-02 23:00:11 +0000

    Small refactor on yesterdays stock price

--- src/main/java/com/malsr/random/YesterdayStockPriceChecker.java
+++ src/main/java/com/malsr/random/YesterdayStockPriceChecker.java
@@ -5,16 +5,14 @@ public class YesterdayStockPriceChecker {
     public int getMaximumProfit(int[] stockPriceYesterday) {
         int overallProfit = Integer.MIN_VALUE;
 
-        for (int i = 0; i <= stockPriceYesterday.length - 1; i++) {
+        for (int i = 0; i < stockPriceYesterday.length; i++) {
             int stockPriceInTime = stockPriceYesterday[i];
-
             int profitMarginForStockPrice = Integer.MIN_VALUE;
-            if (i+1 <= stockPriceYesterday.length-1) {
-                for (int y = i+1; y <= stockPriceYesterday.length - 1; y++) {
-                    int stockPriceInNextMinute = stockPriceYesterday[y];
-                    if (profitMarginForStockPrice < stockPriceInNextMinute - stockPriceInTime) {
-                        profitMarginForStockPrice = stockPriceInNextMinute - stockPriceInTime;
-                    }
+
+            for (int y = i+1; y < stockPriceYesterday.length; y++) {
+                int stockPriceInNextMinute = stockPriceYesterday[y];
+                if (profitMarginForStockPrice < stockPriceInNextMinute - stockPriceInTime) {
+                    profitMarginForStockPrice = stockPriceInNextMinute - stockPriceInTime;
                 }
             }
 
